64e772ab8e9db394b37ddcdaa90408f4ebe05c35
Abhilekha-patibandla <167190400+Abhilekha-patibandla@users.noreply.github.com>
2025-10-29 16:20:16 +0530

    Add files via upload

---
+++ Intro.java
@@ -0,0 +1,44 @@
+package com.typesOfInterfaces;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+//@FunctionalInterface
+//interface A {
+//
+//	void hai();
+//}
+//
+//class B implements A {
+//
+//	@Override
+//	public void hai() {
+//		System.out.println("Hi method");
+//
+//	}
+//
+//}
+
+public class Intro {
+
+	public static void main(String[] args) {
+//		B b = new B();
+//		b.hai();
+		
+//		Consumer<Integer> c= t ->System.out.println(t);
+//		c.accept(100);
+		
+//		Supplier <String> s=()-> "Abhilekha";
+//		System.out.println(s.get());
+		
+//		Predicate<Integer> p= t ->t%2==0;
+//		System.out.println(p.test(100));
+		
+//		Function<String,Integer> f=(String t)->  t.length();
+//		System.out.println(f.apply("Abhilekha"));
+      
+	}
+
+}
